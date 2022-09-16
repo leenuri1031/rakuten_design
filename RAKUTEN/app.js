cf5afd56cdc6a61a912976ebf51cfa462573f25c
leenuri <leenuri1031@gmail.com>
2022-09-16 14:01:19 +0900

    rakuten_mall_renewal_notice

---
+++ RAKUTEN/app.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector('.menu-style');
+// console.log(menu);
+let boolean = true;
+
+  menu.addEventListener('click', ()=>{
+    menu.classList.toggle('toggle');
+    if(boolean === true){
+      
+    }
+  });
+
+
+
