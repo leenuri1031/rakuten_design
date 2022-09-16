d7adbd3a9bcda712c7b7e4b9646ed83ee437fd33
leenuri <leenuri1031@gmail.com>
2022-09-16 17:59:06 +0900

    make_a_slider

--- RAKUTEN/app.js
+++ RAKUTEN/app.js
@@ -1,13 +1,13 @@
-const menu = document.querySelector('.menu-style');
-// console.log(menu);
-let boolean = true;
+// const menu = document.querySelector('.menu-style');
+// // console.log(menu);
+// let boolean = true;
 
-  menu.addEventListener('click', ()=>{
-    menu.classList.toggle('toggle');
-    if(boolean === true){
+//   menu.addEventListener('click', ()=>{
+//     menu.classList.toggle('toggle');
+//     if(boolean === true){
       
-    }
-  });
+//     }
+//   });
 
 
 
